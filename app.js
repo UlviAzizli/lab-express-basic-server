d5bdbdbbb3e8e36d1f56528a3e1308289a306292
Ulvi Azizli <ulviazizli96@gmail.com>
2024-05-18 23:56:52 +0200

    done

--- app.js
+++ app.js
@@ -13,6 +13,25 @@ app.get("/", (req, res) => {
   console.log(req);
   res.sendFile(__dirname + "/views/home.html");
 });
+
+app.get("/blog", (req, res) => {
+  res.sendFile(__dirname + "/views/blog.html");
+});
+
+const projects = require("./data/projects.json");
+app.get("/api/projects", (req, res) => {
+  res.json(projects);
+});
+
+const articles = require("./data/projects.json");
+app.get("/api/articles", (req, res) => {
+  res.json(articles);
+});
+
+app.use("*", (req, res, next) => {
+  res.status(404).sendFile(__dirname + "views/not-found.html");
+});
+
 app.listen(5005, () => {
   console.log("Server listening on port 5005");
 });
